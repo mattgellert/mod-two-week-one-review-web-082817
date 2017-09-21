1f9332ff1b6c42451156f20c943cc539a334c056
mattgellert <mattgellert@gmail.com>
2017-09-21 15:11:46 -0400

    Done.

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,6 +1,41 @@
 class SeasController < ApplicationController
-  #define your controller actions here
+  
+  def index
+    @seas = Sea.all
+  end
 
+  def new
+    @sea = Sea.new
+  end
+
+  def create
+    @sea = Sea.create(sea_params)
+    redirect_to @sea
+  end
+
+  def show
+    @sea = Sea.find(params[:id])
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+  end
+
+  def update
+    @sea = Sea.find(params[:id])
+    @sea.update(sea_params)
+
+    redirect_to @sea
+  end
+
+
+  def destroy
+    @seas = Sea.all
+    @sea = Sea.find(params[:id])
+    @sea.destroy
+
+    redirect_to seas_path
+  end
 
   private
   # In controller actions, use this private method to access sea params from forms.
@@ -8,6 +43,6 @@ class SeasController < ApplicationController
   # check the return value of sea_params to see what you're working with!
   def sea_params
     params.require(:sea).permit(:name, :temperature, :bio, :mood, :image_url, :favorite_color, :scariest_creature, :has_mermaids)
-  end
+  end #whitelists the parameters we are allowed to enter
 
 end
--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,46 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'seas/index'
 
+    resources :seas #defines routes for 7 RESTful actions
+
+  root 'seas#index' #tells Rails to map requests to to /seas/index
 end
+
+
+  #CREATE
+  # get '/seas/new' do
+  #   erb :"seas/new.html"
+  # end
+  #
+  # post '/seas/new' do
+  #   @sea = Sea.create(params[:sea])
+  #   redirect "/layouts/application.html"
+  # end
+
+  #READ
+  # get '/seas' do
+  #   erb :"layouts/application.html"
+  # end
+  #
+  # get '/seas/:id' do
+  #   erb ??
+  # end
+
+  #UPDATE
+  # get '/seas/:id/edit' do
+  #   @sea = Sea.find(params[:id])
+  #   erb ??
+  # end
+  #
+  # patch '/seas/:id/edit' do
+  #   @sea = Sea.find(params[:id])
+  #   @sea.update(params[:sea])
+  #   redirect to ??
+  # end
+
+  #DELETE
+  # delete '/seas/:id/delete' do
+  #   @sea = Sea.find(params[:id])
+  #   @sea.destroy
+  #   erb home
+  # end
